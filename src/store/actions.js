77a62ea989cb295d54313a9ec8dfde73200e8486
luoling <1019483075@qq.com>
2017-09-05 22:46:33 +0800

    测试github

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,6 +2,7 @@ import * as types from './mutation-types'
 export const selectPlay = function({commit, state}, {list, index}) {
   commit(types.SET_SEQUENCE_LIST, list)
  // commit(types.SET_PLAYING, list)
+
   commit(types.SET_CURRENTINDEX, index)
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
