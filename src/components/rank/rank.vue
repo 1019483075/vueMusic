80a44eb29e35489f7ed23e43ca9c37f45c956815
luoling <1019483075@qq.com>
2017-10-11 11:56:04 +0800

    排行页数据抓取

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,15 +1,108 @@
 <template>
-  <div class="rank">
-      rank
+  <div class="rank" ref="rank"><!--设置滚动的属性-->
+    <scroll :data="topList" class="toplist" ref="toplist">
+      <ul>
+        <li class="item" v-for="item in topList">
+          <div class="icon">
+            <img width="100" height="100" v-lazy="item.picUrl"/>
+          </div>
+          <ul class="songlist">
+            <li class="song" v-for="(song,index) in item.songList">
+              <span>{{index+1}}</span>
+              <span>{{song.songname}}-{{song.singername}}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div class="loading-container" v-show="!topList.length">
+        <loading></loading>
+      </div>
+      </scroll>
   </div>
 </template>
 
 <script>
+import Scroll from '../../base/scroll/scroll'
+import {getTopList} from '../../api/rank'
+import {ERR_OK} from '../../api/config'
+import Loading from '../../base/loading/loading'
+import {playlistMixin} from '../../common/js/mixin'
 export default {
-  name: 'rank'
+  name: 'rank',
+  mixins: [playlistMixin],
+  data() {
+    return {
+      topList: []
+    }
+  },
+  created() {
+    // 增加延时可以方便看到loading
+    // setTimeout(() => {
+    //   this._getTopList()
+    // }, 1000)
+    this._getTopList()
+  },
+  methods: {
+    handlePlayList(playList) {
+      const bottom = playList.length > 0 ? '60px' : ''
+      this.$refs.rank.style.bottom = bottom
+      this.$refs.toplist.refresh()// 刚发生变化的时候就刷新
+    },
+    _getTopList() {
+      getTopList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.topList = res.data.topList
+          // console.log(this.topList)
+        }
+      })
+    }
+  },
+  components: {
+    Scroll,
+    Loading
+  }
 }
 </script>
 
-<style>
-  
+<style lang="stylus" rel="stylesheet/stylus" scoped="scoped">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+.rank
+  position :fixed
+  width :100%
+  top:88px
+  bottom :0
+  .toplist
+    height :100%
+    overflow :hidden
+    .item
+      display :flex
+      margin:0 20px
+      padding-top:20px
+      height :100px
+    &:last-child
+      padding-bottom :20px
+    .icon
+      flex :0 0 100px
+      width :100px
+      height :100px
+    .songlist
+      flex :1
+      display :flex
+      flex-direction :column
+      justify-content :center
+      padding :0 20px
+      height :100px
+      overflow :hidden
+      background: $color-highlight-background
+      color: $color-text-d
+      font-size: $font-size-small
+      .song
+        no-wrap()
+        line-height :26px
+    .loading-container
+      position :absolute
+      width :100%
+      top:50%
+      transform :translateY(-50%)
 </style>
