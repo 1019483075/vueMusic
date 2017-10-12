1c6d9dc7bc5780ccf206891fbe031e328c4175dc
luoling <1019483075@qq.com>
2017-10-12 15:24:11 +0800

    搜索检索组件初始化

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="suggest">
+  <div class="suggest"><!--此处是搜索检索的列表页面-->
     <ul class="suggest-list">
-      <li class="suggest-item">
+      <li class="suggest-item" v-for="item in result">
         <div class="icon">
-          <i></i>
+          <i :class="getIconCls"></i>
         </div>
         <div class="name">
-          <p class="text"></p>
+          <p class="text" v-html="getDisplayName(item)"></p>
         </div>
       </li>
       <loading></loading>
@@ -16,31 +16,69 @@
 </template>
 
 <script>
+import {search} from '../../api/search'
+import {ERR_OK} from '../../api/config'
+const TYPE_SINGER = 'singer'
+import {filterSinger} from '../../common/js/song'
 export default {
   name: 'suggest',
   // 此组件是依赖于检索词的
   props: {
-    query:{
-      type:String
-      default:''
+    query: {
+      type: String,
+      default: ''
+    },
+    showSinger: {// 是否显示歌手
+      type: Boolean,
+      default: true
     }
   },
   data() {
     return {
-
-    }
-  }
-  methods:{
-    search(){
-
+      page: 1, // 页数
+      result: []// 用于接受数据
     }
   },
-  watch:{
-    query(){
+  methods: {
+    search() {
+      search(this.query, this.page, this.showSinger).then((res) => {
+        if (res.code === ERR_OK) {
+          this.result = this._genResult(res.data)
+          console.log(res.data)
+        }
+      })
+    },
+    _genResult(data) {
+      let ret = []
+      if (data.zhida && data.zhida.singerid) {
+        ret.push({...data.zhida, ...{type: TYPE_SINGER}})
+      }
+      if (data.song) {
+        ret = ret.concat(data.song.list)
+      }
+      return ret
+    },
+    getIconCls(item) {
+      if (item.type === TYPE_SINGER) {
+        return 'icon-mine'
+      } else {
+        return 'icon-music'
+      }
+    },
+    getDisplayName(item) {
+      if (item.type === TYPE_SINGER) {
+        return item.singername
+      } else {
+        return `${item.name}-${item.singer}`
+      }
+    }
+  },
+  watch: {
+    query() {
       this.search()
     }
   }
-  
+
 }
 </script>
 
